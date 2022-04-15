7fd5fcaa21f191ca9f1b76d0985e3be037786515
Abhishekar0308 <88946903+Abhishekar0308@users.noreply.github.com>
2022-04-15 18:41:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+mustacheX=0;
+mustacheY=0;
+
+function preload() 
+{mustache=loadImage('m.png');}
+
+function setup() {
+
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+console.log('PoseNet Is  Initialized');
+
+}
+function gotPoses (results) 
+{
+
+    if(results.length >0)
+    {
+        console.log(results);
+        console.log("mustache x ="+results[0].pose.mustache.x);
+        console.log("mustache y ="+results[0].pose.mustache.y);
+       
+        mustacheX=results[0].pose.mustache.x;
+        mustacheY=results[0].pose.mustache.y;
+    }
+}
+
+function draw() 
+{
+   
+image(mustache,mustacheX,mustacheY,200,200 );
+}
+
+function take_snapshot(){
+
+    save('Abhishek.png');
+
+}
